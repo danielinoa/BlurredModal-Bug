4b6a17d386c50f91c1f586c375deee0ccd5bfa4e
Daniel Inoa <danielinoa@gmail.com>
2016-10-26 00:12:25 -0400

    Sets up project

---
+++ BlurredModal/BlurredModal/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  BlurredModal
+//
+//  Created by Daniel Inoa on 10/25/16.
+//  Copyright © 2016 Daniel Inoa. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+    
+}
---
+++ BlurredModal/BlurredModal/BlurredModalViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BlurredModalViewController.swift
+//  BlurredModal
+//
+//  Created by Daniel Inoa on 10/25/16.
+//  Copyright © 2016 Daniel Inoa. All rights reserved.
+//
+
+import UIKit
+
+class BlurredModalViewController: UIViewController {
+
+    @IBAction func dismiss(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
